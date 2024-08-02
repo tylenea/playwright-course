c1903383d44d700682252a2ff6cc5d21b9c9398b
unknown <animetylenea@gmail.com>
2024-08-03 02:55:08 +0300

    feat: update components with #features

--- components/OurStoryComponent.ts
+++ components/OurStoryComponent.ts
@@ -1,36 +1,25 @@
-import {expect, Page} from "@playwright/test"
+import { Page } from '@playwright/test';
 
 class OurStoryComponent {
-    constructor(private page: Page) {
+  constructor(private page: Page) {
     this.page = page;
-    }
-    
-    async getHeading (){
-        await expect(this.page.getByRole('heading', { name: 'Our story' })).toBeVisible();
-    };
+  }
 
-   async getAvatar (){
-        await expect(this.page.locator('li')
-        .filter({ hasText: 'Ava Sandler' })
-        .locator('div').first())
-        .toBeVisible();
-  
-        await expect(this.page.locator('li')
-        .filter({ hasText: 'Steph Poco' })
-        .locator('div').first())
-        .toBeVisible();
-    };
+  getAvatar(name: string) {
+    return this.page.locator('li').filter({ hasText: name }).locator('div').first();
+  }
 
-    async getParagraph (){
-        await expect(this.page.getByText('What more could you want from')).toBeVisible();
-        await expect(this.page.getByText('It\'s easy to forget that we\'')).toBeVisible();
-        await expect(this.page.getByText('We like to keep things plain')).toBeVisible();
-    }
-    async getParagraphHeading(){
-        await expect(this.page.getByRole('heading', { name: 'Passion' })).toBeVisible();
-        await expect(this.page.getByRole('heading', { name: 'Animal' })).toBeVisible();
-        await expect(this.page.getByRole('heading', { name: 'Style' })).toBeVisible();
-   
-    }
+  getEmployeeName(name: string) {
+    return this.page.getByText(name);
+  }
+
+  getMotivationParagraph(heading: string) {
+    return this.page.getByRole('heading', { name: heading });
+  }
+
+  getMotivationText(text: string) {
+    return this.page.getByText(text);
+  }
 }
-export {OurStoryComponent}
+
+export { OurStoryComponent };
